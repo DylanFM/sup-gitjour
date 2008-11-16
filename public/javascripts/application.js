0c7c84a82d61f083ed456ce6bd081038573271b3
DylanFM <dylan.fm@gmail.com>
2008-11-16 13:41:01 +1100

    Text inputs selected.

--- public/javascripts/application.js
+++ public/javascripts/application.js
@@ -1,3 +1,11 @@
+function setup_jours ()
+{
+  $("input.url").focus(function() 
+  {
+    this.select();
+  });
+}
+
 function update_jours ()
 {
 	$.getJSON('/gitjours/list.json',{},
@@ -9,15 +17,17 @@ function update_jours ()
       var app = json[i];
       apps += '<li><strong>'+app.name+'</strong><ul class="repos">';
       $(app.repos).each(function(j){
-        apps += '<li class="repo">'+app.repos[j].name+' <a href="'+app.repos[j].url+'" title="git clone '+app.repos[j].url+'" class="url">'+app.repos[j].url+'</a></li>';
+        apps += '<li class="repo">'+app.repos[j].name+' <input type="text" value="'+app.repos[j].url+'" class="url" size="50" readonly="readonly" /></li>';
       });
       apps += '</ul></li>';
     }
     $("#apps").html(apps);
+    setup_jours();
     setTimeout(update_jours, 6000);
   });
 }
 
 $(document).ready(function() {
+  setup_jours();
   update_jours();
 });
